9b50bbfa0c454c05136a8522c2851a1a13854b24
Karl Ward <kw1213@nyu.edu>
2012-12-18 07:26:44 -0500

    updated example to include two sensors --KW

--- examples/SuperSensorExample/SuperSensorExample.ino
+++ examples/SuperSensorExample/SuperSensorExample.ino
@@ -3,12 +3,9 @@
  * 
  * Simple example demonstrating the "SuperSensor" Arduino library
  * 
- * This code reads an analog sensor and outputs the current, mean,  
- * and median values to the Arduino hardware serial port.  
+ * This code reads two sensors (one analog, one digital) and outputs the 
+ * current, mean, and median values to the Arduino hardware serial port.  
  *
- * Remember that this is a simple example.  You can use more than one 
- * SuperSensor in your own code. 
- * 
  * Author: Karl Ward
  * License: this example code is released into the public domain
  * (note that the SuperSensor library itself is not public domain)
@@ -17,22 +14,36 @@
 
 #include "SuperSensor.h"
 
-// we're going to do analogRead on pin A0, storing 10 values for analysis
-SuperSensor sensor(A0, SUPERSENSOR_DIGITAL, 10); 
+// we're going to do analogRead on pin A0, storing 100 values for analysis
+SuperSensor analogSensor(A0, SUPERSENSOR_ANALOG, 100); 
+
+// we're going to do digitalRead on pin 2, storing 10 values for analysis
+SuperSensor digitalSensor(2, SUPERSENSOR_DIGITAL, 10); 
 
 void setup() { 
   Serial.begin(9600); // open serial port for debugging output
 } 
 
 void loop() { 
-  int currentValue = sensor.read();
-  int meanValue = sensor.mean(); 
-  int medianValue = sensor.median(); 
-  Serial.print("current value is "); 
-  Serial.print(currentValue); 
+  int analogValue = analogSensor.read();
+  int analogMean = analogSensor.mean(); 
+  int analogMedian = analogSensor.median(); 
+  Serial.print("analog sensor value is "); 
+  Serial.print(analogValue); 
   Serial.print(", mean is "); 
-  Serial.print(meanValue); 
+  Serial.print(analogMean); 
   Serial.print(", median is "); 
-  Serial.println(medianValue); 
+  Serial.println(analogMedian);
+
+  int digitalValue = digitalSensor.read();
+  int digitalMean = digitalSensor.mean(); 
+  int digitalMedian = digitalSensor.median(); 
+  Serial.print("digital sensor value is "); 
+  Serial.print(digitalValue); 
+  Serial.print(", mean is "); 
+  Serial.print(digitalMean); 
+  Serial.print(", median is "); 
+  Serial.println(digitalMedian);  
+  
   delay(250); // short delay so we don't flood the serial monitor
 }
